b3b0bb405f613db94522d7b2299d0deaa2ed1c31
ranieljumz <raniel.jumamil@scribblecubes.ph>
2025-09-19 04:54:24 +0800

    Update middlewares.js

--- config/middlewares.js
+++ config/middlewares.js
@@ -15,7 +15,18 @@ module.exports = [
       },
     },
   },
-  "strapi::cors",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      // Your Nuxt frontend's production and development URLs
+      origin: ["http://localhost:3000", "https://sc-shelfie-web-751939145235.asia-southeast1.run.app"],
+      // --- THIS IS THE CRITICAL ADDITION ---
+      // This tells the browser that it's safe to send cookies to Strapi.
+      credentials: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
