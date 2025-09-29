ad2da63cdf1ebd483b9fec216faf91463fd4e5fe
ranieljumz <raniel.jumamil@scribblecubes.ph>
2025-09-29 18:38:07 +0800

    Tag Vote system

---
+++ src/api/user-tag-vote/controllers/user-tag-vote.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * user-tag-vote controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::user-tag-vote.user-tag-vote');
